653eda20318aae716467c02f3b6acd269be994cc
ngthanhtin <ngthanhtinqn@gmail.com>
2022-04-13 15:44:30 +0900

    add args to train simple ae

--- l2f/common/models/simple_ae.py
+++ l2f/common/models/simple_ae.py
@@ -13,6 +13,8 @@ import os, random
 from PIL import Image
 import numpy as np
 
+import argparse
+
 def add_noise(images, mean=0, std=0.1):
     normal_dst = Normal(mean, std)
     noise = normal_dst.sample(images.shape)
@@ -51,11 +53,11 @@ class Auto_Encoder_Model(nn.Module):
         output = self.reconstruct_pass(output)
         return output
 
-def train():
+def train(args):
     #train AE
     device = 'cuda:0'
-    im_channel = 1
-    im_w = 224
+    im_channel = args.im_channels
+    im_w = args.sizes
     model = Auto_Encoder_Model(im_c=im_channel).to(device)
     
     # model.load_state_dict(torch.load('./ae/ae_full_prelu.pth'))
@@ -65,7 +67,7 @@ def train():
     #load data
     # ToTensor already divided the image by 255
     batch_size = 16
-    dataset = datasets.ImageFolder('/home/tinvn/TIN/Agileflight_pictures/Depth/',\
+    dataset = datasets.ImageFolder(args.data,\
          transform=transforms.Compose([transforms.Resize((224, 224)), transforms.Grayscale(), transforms.ToTensor()]))
     data_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
     #train
@@ -90,22 +92,27 @@ def train():
 
     torch.save(model.state_dict(),'simple_ae_{}_depth_{}_size.pth'.format(im_channel, im_w))
 
-def test():
+def test(args):
     device = 'cuda:0'
-    model = Auto_Encoder_Model().to(device)
-    weight_path = 'simple_ae_224.pth'
+    model = Auto_Encoder_Model(im_c=args.im_channels).to(device)
+    weight_path = args.weight_path
     print('load initial weights from: %s'%(weight_path))
     model.load_state_dict(torch.load(weight_path))
     model.eval()
-    folder = '/home/tinvn/TIN/Agileflight_pictures/RGB/images/'
+    folder = args.data
     files = os.listdir(folder)
     f = random.choice(files)
 
     image_path = folder + f
-    # image_path = "/home/tinvn/Desktop/14167.png"
-    image = cv2.imread(image_path)
-    image = cv2.resize(image, (224, 224))
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if args.im_channels == 1:
+        image = cv2.imread(image_path, 0)
+        image = cv2.resize(image, (224, 224))
+    else:
+        image = cv2.imread(image_path)
+        image = cv2.resize(image, (224, 224))
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    
+    # normalize
     image_pil = Image.fromarray(image)
     image = image/255
     # print(image)
@@ -113,6 +120,8 @@ def test():
     
     # to_tensor = transforms.ToTensor()
     image = torch.Tensor(image)
+    if args.im_channels == 1:
+        image = image.unsqueeze(-1)
     image = image.permute(-1,0,1)
     image = image.unsqueeze(0)
     
@@ -121,18 +130,29 @@ def test():
     image = image.to(device)
     encoder = model.forward_pass(image)
     decoder = model.reconstruct_pass(encoder)
-    decoder = decoder.squeeze()
+    decoder = decoder.squeeze(0)
     decoder = decoder.permute(1, 2, 0)
     
     f = plt.figure()
     f.add_subplot(1,1, 1) #1,2,1
     plt.imshow(decoder.cpu().detach().numpy())
 
-    # f.add_subplot(1, 2, 2)
-    # plt.imshow(image_pil)
-
     print(time.time()-t)
     plt.show()
 
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train", type=int, default=1, help="1: Train, 0: Evaluate")
+    parser.add_argument("--im_channels", type=int, default=1, help="3: RGB, 1: Depth")
+    parser.add_argument("--sizes", type=int, default=224, help="Image width (height)")
+    parser.add_argument("--data", type=str, default="/home/tinvn/TIN/Agileflight_pictures/Depth/images/", help="data folder")
+    parser.add_argument("--weight_path", type=str, default="simple_ae_1_depth_224_size.pth", help="weight path")
+    parser.add_argument("--render", type=int, default=0, help="1: visualize")
+    return parser
+
 if __name__ == "__main__":
-    train()
+    args = parser().parse_args()
+    if args.train:
+        train(args)
+    else:
+        test(args)
