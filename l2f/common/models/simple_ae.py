fix args and fix kld to mean to fix overflow

l2f/common/models/simple_ae.py
@@ -60,7 +60,8 @@ def train(args):
     im_w = args.sizes
     model = Auto_Encoder_Model(im_c=im_channel).to(device)
     
-    # model.load_state_dict(torch.load('./ae/ae_full_prelu.pth'))
+    if args.continue_training:
+        model.load_state_dict(torch.load(args.load_weight_path))
     ae_criterion = torch.nn.MSELoss().to(device)
     optim = torch.optim.Adamax(filter(lambda p: p.requires_grad, model.parameters()))
 
@@ -90,12 +91,12 @@ def train(args):
             current_loss += loss
         print('{} loss : {}'.format(epoch+1, batch_size*current_loss/len(data_loader)))
 
-    torch.save(model.state_dict(),'simple_ae_{}_depth_{}_size.pth'.format(im_channel, im_w))
+    torch.save(model.state_dict(),'{}'.format(args.save_weight_path))
 
 def test(args):
     device = 'cuda:0'
     model = Auto_Encoder_Model(im_c=args.im_channels).to(device)
-    weight_path = args.weight_path
+    weight_path = args.load_weight_path
     print('load initial weights from: %s'%(weight_path))
     model.load_state_dict(torch.load(weight_path))
     model.eval()
@@ -147,8 +148,10 @@ def parser():
     parser.add_argument("--im_channels", type=int, default=1, help="3: RGB, 1: Depth")
     parser.add_argument("--sizes", type=int, default=224, help="Image width (height)")
     parser.add_argument("--data", type=str, default="/home/tinvn/TIN/Agileflight_pictures/Depth/", help="data folder")
-    parser.add_argument("--weight_path", type=str, default="simple_ae_1_depth_224_size.pth", help="weight path")
     parser.add_argument("--render", type=int, default=0, help="1: visualize")
+    parser.add_argument("--load_weight_path", type=str, default="simple_ae_1_depth_224_size.pth", help="weight path")
+    parser.add_argument("--save_weight_path", type=str, default="simple_ae_1_depth_224_size.pth", help="weight path")
+    parser.add_argument("--continue-training", type=int, default="1", help="continue training with a pretrained weight")
     return parser
 
 if __name__ == "__main__":

l2f/common/models/vae.py
@@ -17,6 +17,8 @@ import torch.nn.functional as F
 from torchvision import datasets, transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 
+import argparse
+
 class VAE(nn.Module):
     """Input should be (bsz, C, H, W) where C=3, H=42, W=144"""
 
@@ -27,27 +29,27 @@ class VAE(nn.Module):
         self.im_h = im_h
         self.im_w = im_w
 
-        # encoder_list = [
-        #     nn.Conv2d(im_c, 32, kernel_size=4, stride=2, padding=1),
-        #     nn.ReLU(),
-        #     nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
-        #     nn.ReLU(),
-        #     nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
-        #     nn.ReLU(),
-        #     nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
-        #     nn.ReLU(),
-        #     nn.Flatten(),
-        # ]
-
         encoder_list = [
-            nn.Conv2d(im_c, 128, kernel_size=8, stride=4),
+            nn.Conv2d(im_c, 32, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.Conv2d(128, 64, kernel_size=4, stride=2),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=4, stride=2),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
             nn.Flatten(),
         ]
+
+        # encoder_list = [
+        #     nn.Conv2d(im_c, 128, kernel_size=8, stride=4),
+        #     nn.ReLU(),
+        #     nn.Conv2d(128, 64, kernel_size=4, stride=2),
+        #     nn.ReLU(),
+        #     nn.Conv2d(64, 64, kernel_size=4, stride=2),
+        #     nn.ReLU(),
+        #     nn.Flatten(),
+        # ]
 
         self.encoder = nn.Sequential(*encoder_list)
         sample_img = torch.zeros([1, im_c, im_h, im_w])
@@ -57,43 +59,43 @@ class VAE(nn.Module):
         self.fc2 = nn.Linear(h_dim, z_dim)
         self.fc3 = nn.Linear(z_dim, h_dim)
 
-        # self.decoder = nn.Sequential(
-        #     nn.Unflatten(1, em_shape),
-        #     nn.ConvTranspose2d(
-        #         em_shape[0],
-        #         128,
-        #         kernel_size=4,
-        #         stride=2,
-        #         padding=1,
-        #         output_padding=(0, 0),
-        #     ),
-        #     nn.ReLU(),
-        #     nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
-        #     nn.ReLU(),
-        #     nn.ConvTranspose2d(
-        #         64, 32, kernel_size=4, stride=2, padding=1, output_padding=(0, 0)
-        #     ),
-        #     nn.ReLU(),
-        #     nn.ConvTranspose2d(32, im_c, kernel_size=4, stride=2, padding=1),
-        #     nn.Sigmoid(),
-        # )
-
         self.decoder = nn.Sequential(
             nn.Unflatten(1, em_shape),
             nn.ConvTranspose2d(
                 em_shape[0],
-                64,
+                128,
                 kernel_size=4,
                 stride=2,
+                padding=1,
+                output_padding=(0, 0),
             ),
             nn.ReLU(),
-            nn.ConvTranspose2d(64, 128, kernel_size=4, stride=2, output_padding=[1, 1]),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
             nn.ConvTranspose2d(
-                128, im_c, kernel_size=8, stride=4
+                64, 32, kernel_size=4, stride=2, padding=1, output_padding=(0, 0)
             ),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, im_c, kernel_size=4, stride=2, padding=1),
             nn.Sigmoid(),
         )
+
+        # self.decoder = nn.Sequential(
+        #     nn.Unflatten(1, em_shape),
+        #     nn.ConvTranspose2d(
+        #         em_shape[0],
+        #         64,
+        #         kernel_size=4,
+        #         stride=2,
+        #     ),
+        #     nn.ReLU(),
+        #     nn.ConvTranspose2d(64, 128, kernel_size=4, stride=2, output_padding=[1, 1]),
+        #     nn.ReLU(),
+        #     nn.ConvTranspose2d(
+        #         128, im_c, kernel_size=8, stride=4
+        #     ),
+        #     nn.Sigmoid(),
+        # )
 
     def reparameterize(self, mu, logvar):
         std = logvar.mul(0.5).exp_()
@@ -141,28 +143,27 @@ class VAE(nn.Module):
 
     def loss(self, actual, recon, mu, logvar, kld_weight=1.0):
         bce = F.binary_cross_entropy(recon, actual, reduction="mean")        
-        kld = -0.5 * torch.sum(1 + logvar - mu ** 2 - logvar.exp())
+        kld = -0.5 * torch.mean(1 + logvar - mu ** 2 - logvar.exp())
 
         return bce + kld * kld_weight
 
-def test():
+def test(args):
     device = 'cuda:2'
-    model = VAE(im_c=3, im_h=144, im_w=144, z_dim=32).to(device)
-    weight_path = '/home/tinvn/TIN/Drone_Challenge/src/agile_flight/envtest/python2/l2f/common/models/vae.pth'
+    model = VAE(im_c=args.im_channels, im_h=args.sizes, im_w=args.sizes, z_dim=args.z_dim).to(device)
+    weight_path = args.load_weight_path
     print('load initial weights from: %s'%(weight_path))
     model.load_state_dict(torch.load(weight_path))
     model.eval()
-    folder = '/home/tinvn/TIN/Agileflight_pictures/RGB/images/'
+    folder = args.data + "/images/"
     files = os.listdir(folder)
     f = random.choice(files)
 
     image_path = folder + f
-    # image_path = "/home/tinvn/Desktop/14167.png"
     image = cv2.imread(image_path)
-    image = cv2.resize(image, (144, 144))
+    image = cv2.resize(image, (args.sizes, args.sizes))
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     image_pil = Image.fromarray(image)
-    # image = image/255
+    image = image/255
     # print(image)
     
     
@@ -172,40 +173,38 @@ def test():
     image = image.unsqueeze(0)
     
     image = image.to(device)
-    z, mu, logvar = model.encode(image)
-    print(z.shape)
-    decoder = model.decode(z)
-    decoder = decoder.squeeze()
+    z, mu, logvar = model.forward(image)
+    decoder = z.squeeze()
     decoder = decoder.permute(1, 2, 0)
     
     f = plt.figure()
     f.add_subplot(1,1, 1) #1,2,1
     plt.imshow(decoder.cpu().detach().numpy())
 
-    # f.add_subplot(1,2, 2)
-    # plt.imshow(image_pil)
     plt.show()
 
-def train():
+def train(args):
     #train AE
     device = 'cuda:2'
-    img_w, img_h = 224, 224
-    z_dim = 128
-    im_channel = 3
+    img_w, img_h = args.sizes, args.sizes
+    z_dim = args.z_dim
+    im_channel = args.im_channels
     model = VAE(im_c=im_channel, im_h=img_h, im_w=img_w, z_dim=z_dim).to(device)
     
-    # model.load_state_dict(torch.load('/home/tinvn/TIN/Drone_Challenge/src/agile_flight/envtest/python2/l2f/common/models/vae.pth'))
-    lr = 1e-1
-    optim = torch.optim.Adamax(filter(lambda p: p.requires_grad, model.parameters()))
+    if args.continue_training:
+        model.load_state_dict(torch.load(args.load_weight_path))
+    #hyper params
+    lr = 1e-4
+    batch_size = 16
+    epochs = 10
+    
     optim = torch.optim.Adam(model.parameters(), lr=lr)
     #load data
     # ToTensor already divided the image by 255
-    batch_size = 16
-    dataset = datasets.ImageFolder('/home/tinvn/TIN/Agileflight_pictures/RGB/', \
+    dataset = datasets.ImageFolder(args.data, \
          transform=transforms.Compose([transforms.Resize((img_w, img_h)), transforms.ToTensor()]))
     data_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
-    #train
-    epochs = 100
+    #
     for epoch in range(0, epochs):
         current_loss = 0
         for i,data in enumerate(data_loader,0):
@@ -215,7 +214,7 @@ def train():
             optim.zero_grad()
             decode, mu, logvar = model.forward(input)
             
-            loss = model.loss(input, decode, mu, logvar, kld_weight=0.0) 
+            loss = model.loss(input, decode, mu, logvar, kld_weight=args.kld_weight) 
             loss.backward()
             optim.step()
 
@@ -223,8 +222,25 @@ def train():
             current_loss += loss
         print('{} loss : {}'.format(epoch+1, batch_size*current_loss/len(data_loader)))
 
-    torch.save(model.state_dict(), "vae{}.pth".format(img_w))
-        
+    torch.save(model.state_dict(), "{}".format(args.save_weight_path))
+
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train", type=int, default=1, help="1: Train, 0: Evaluate")
+    parser.add_argument("--im_channels", type=int, default=3, help="3: RGB, 1: Depth")
+    parser.add_argument("--sizes", type=int, default=224, help="Image width (height)")
+    parser.add_argument("--z-dim", type=int, default=128, help="Latent dims")
+    parser.add_argument("--kld-weight", type=float, default=0.0, help="KL Divergence weight")
+    parser.add_argument("--data", type=str, default="/home/tinvn/TIN/Agileflight_pictures/RGB/", help="data folder")
+    parser.add_argument("--render", type=int, default=0, help="1: visualize")
+    parser.add_argument("--load_weight_path", type=str, default="vae224.pth", help="weight path")
+    parser.add_argument("--save_weight_path", type=str, default="vae224.pth", help="weight path")
+    parser.add_argument("--continue-training", type=int, default="1", help="continue training with a pretrained weight")
+    return parser
 
 if __name__ == "__main__":
-    train()
+    args = parser().parse_args()
+    if args.train:
+        train(args)
+    else:
+        test(args)